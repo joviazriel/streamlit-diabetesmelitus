184e6b49efedd20ee5803247d77cb74fedbb911f
joviazriel <104018425+joviazriel@users.noreply.github.com>
2023-06-25 13:46:23 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -11,28 +11,28 @@ st.title('Prediksi Penyakit Diabetes Melitus ')
 col1, col2 = st.columns(2)
 
 with col1 :
-    Pregnancies = st.text_input ('input nilai Pregnancies')
+    Pregnancies = st.text_input ('input nilai Kehamilan')
 
 with col2 :
-    Glucose = st.text_input ('input nilai Glucose')
+    Glucose = st.text_input ('input nilai Gula Darah ')
 
 with col1:
-    BloodPressure = st.text_input ('input nilai Blood Pressure')
+    BloodPressure = st.text_input ('input nilai Tekanan Darah')
 
 with col2 :
-    SkinThickness = st.text_input ('input nilai Skin Thickness')
+    SkinThickness = st.text_input ('input nilai Ketebalan Kulit')
 
 with col1:
     Insulin = st.text_input ('input nilai Insulin')
 
 with col2 :
-    BMI = st.text_input ('input nilai BMI')
+    BMI = st.text_input ('input nilai Body Massa Index')
 
 with col1:
-    DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
+    DiabetesPedigreeFunction = st.text_input ('input nilai Riwayat Diabetes 0 - 2')
 
 with col2 :
-    Age = st.text_input ('input nilai Age')
+    Age = st.text_input ('input nilai Umur')
 
 # code untuk prediksi
 diab_diagnosis = ''
