a0fc7a3f70d204686ec63fcf3e80164b578bda3f
joviazriel <104018425+joviazriel@users.noreply.github.com>
2023-06-25 14:16:10 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -41,7 +41,7 @@ diab_diagnosis = ''
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-    if(diab_prediction[0] == 1):
+    if(diab_prediction[0] == 0):
         diab_diagnosis = 'Pasien terkena Diabetes'
     else :
         diab_diagnosis = 'Pasien tidak terkena Diabetes'
