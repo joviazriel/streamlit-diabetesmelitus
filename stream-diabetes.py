942d4c55909b8c7ddd57f8821fd04ac740e9ec6e
joviazriel <104018425+joviazriel@users.noreply.github.com>
2023-06-25 14:03:30 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -17,13 +17,13 @@ with col2 :
     Glucose = st.text_input ('input nilai Gula Darah ( 70 - 99 )')
 
 with col1:
-    BloodPressure = st.text_input ('input nilai Tekanan Darah')
+    BloodPressure = st.text_input ('input nilai Tekanan Darah ( 90 - 120 / 60 - 90 )')
 
 with col2 :
-    SkinThickness = st.text_input ('input nilai Ketebalan Kulit')
+    SkinThickness = st.text_input ('input nilai Ketebalan Kulit ( 1 - 50 )')
 
 with col1:
-    Insulin = st.text_input ('input nilai Insulin')
+    Insulin = st.text_input ('input nilai Insulin ( 0 - 500 )')
 
 with col2 :
     BMI = st.text_input ('input nilai Body Massa Index ( 18.5 - 25 )')
